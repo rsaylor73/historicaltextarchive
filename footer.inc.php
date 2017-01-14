f582e3790948851913080328794a33454e714414
Robert Saylor <robert@customphpdesign.com>
2017-01-14 14:27:02 -0500

    added footer

---
+++ footer.inc.php
@@ -0,0 +1,26 @@
+</blockquote>
+</font>
+</td>
+          
+
+<table width="1250" border="0" bgcolor="#FFFFFF">
+  <tr> 
+    
+    <td width="466" align="center" colspan="7"><br>
+    </td>
+    
+  </tr>
+</table>
+<table width="1250" bgcolor="white" border="0" class=footer><tr><td><p>&copy 1990-<?php echo date("Y");?>
+
+
+        <a href="about.php">Donald J. Mabry / The Historical Text Archive</a>&nbsp;|&nbsp;<a href="http://www.customphpdesign.com" target=_blank>Programmed By: Custom PHP Design</a></font></p>
+      <p>
+
+	
+		<br>
+      </p></td></tr>
+      </table>
+    </center>
+</body>
+</html>
